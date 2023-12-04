8e630fe114294528ed6e5b700ff61d198b53b811
thnkieu <thienkieu611@gmail.com>
2023-12-04 21:35:38 +0700

    buoi5_router_form

--- src/index.js
+++ src/index.js
@@ -1,44 +1,48 @@
 import ReactDOM from "react-dom/client";
-import { Card } from "./components/Card";
-import { AlertComponent } from "./components/AlertComponent";
-import HeaderHome from "./components/HeaderHome";
-import HomeLayout from "./components/HomeLayout/HomeLayout";
-import DataBinding from "./DataBinding/DataBinding";
 
 //import scss ở index (Vì import ở bất kì file nào khi build vẫn ra index.html)
 //Tạo root trên thẻ div#root
 import "./assets/scss/style.scss";
-import StyleComponent from "./StyleComponent/StyleComponent";
-import HandleEvent from "./HandleEvent/HandleEvent";
-import ChangeFontSize from "./StateDemo/ChangeFontSize";
-import ChangeColor from "./StateDemo/ChangeColor";
-import ChangeAvatar from "./StateDemo/ChangeAvatar";
-import ChangeCarColor from "./StateDemo/ChangeCarColor";
-import RenderWithMap from "./RenderWithMap/RenderWithMap";
-import DemoProps from "./Props/DemoProps/DemoProps";
-import EXRenderProductList from "./Props/EXRenderProductList/EXRenderProductList";
-import BaiTapXemChiTiet from "./Props/BaiTapXemChiTiet/BaiTapXemChiTiet";
+
+//cấu hình router dom
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+  Navigate,
+} from "react-router-dom";
+
 import BaiTapGioHang from "./Props/BaiTapGioHang/BaiTapGioHang";
+import ChangeColor from "./StateDemo/ChangeCarColor";
+import Header from "./components/Header";
+import HomeTemplate from "./templates/HomeTemplate";
+import Login from "./Pages/Login";
+import UserTemplate from "./templates/UserTemplate";
+import Register from "./Pages/Register";
+import Page404 from "./Pages/Page404";
+import Admin from "./Pages/Admin";
+import ReactForm from "./Pages/ReactForm/ReactForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //JSX
 root.render(
-  // <div className="abc">
-  //   {/* <HomeLayout/> */}
-  //   <DataBinding />
-  //   <p className="text-red">demo scss</p>
-  //   <StyleComponent />
-  // </div>
-  <div className="abc">
-    {/* <HandleEvent />
-    <ChangeFontSize />
-    <ChangeColor /> */}
-    {/* <ChangeAvatar /> */}
-    {/* <ChangeCarColor /> */}
-    {/* <RenderWithMap /> */}
-    {/* <DemoProps /> */}
-    {/* <EXRenderProductList /> */}
-    {/* <BaiTapXemChiTiet /> */}
-    <BaiTapGioHang />
-  </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="" element={<HomeTemplate />}>
+        <Route index element={<div>Home</div>}></Route>
+        <Route path="gio-hang" element={<BaiTapGioHang />}></Route>
+        <Route path="bt-change-color" element={<ChangeColor />}></Route>
+        <Route path="react-form" element={<ReactForm />}></Route>
+      </Route>
+      <Route path="user" element={<UserTemplate />}>
+        <Route index element={<Login />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="register" element={<Register />}></Route>
+      </Route>
+      <Route path="admin" element={<Admin />}></Route>
+
+      <Route path="*" element={<Navigate to="" />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
